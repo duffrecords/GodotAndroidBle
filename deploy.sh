9289c503a7aee6839106e1827da64f84da8c5e3a
Ian Young <ian@duffrecords.com>
2025-11-11 22:14:08 -0800

    move everything to the root folder for simplicity

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp -a godotandroidble/blessed/build/outputs/aar/Blessed.aar /srv/apps/godot/bike-vr/android/plugins/godotandroidble/blessed-debug.aar
+cp -a godotandroidble/app/build/outputs/aar/GodotAndroidBle.aar /srv/apps/godot/bike-vr/android/plugins/godotandroidble/godotandroidble-debug.aar
+
