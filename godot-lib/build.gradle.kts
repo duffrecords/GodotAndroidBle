e668468dae98aa2c71ed1218c88d97f7cf08549d
Ian Young <ian@duffrecords.com>
2025-11-14 14:38:42 -0800

    add placeholder for Godot library

---
+++ godot-lib/build.gradle.kts
@@ -0,0 +1,6 @@
+// This file will be overwritten by CI/CD workflows
+// The workflows will download the appropriate Godot AAR file and update this configuration
+// For local development, you need to manually download the Godot AAR file or run the CI setup
+
+configurations.maybeCreate("default")
+artifacts.add("default", file('godot-lib.4.4.1.stable.template_release.aar'))
