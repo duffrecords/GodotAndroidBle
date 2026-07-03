b3a18c549b2f524df6bef1464cd49e90d60f78a4
Ian Young <ian@duffrecords.com>
2026-07-03 14:52:24 -0700

    support for Godot 4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(project(":blessed"))
     compileOnly(project(":godot-lib"))
-    //implementation("org.godotengine:godot:4.6.2.stable")
+    //implementation("org.godotengine:godot:4.7.0.stable")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
